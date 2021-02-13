7be47c4da4f705276cd13e6012e53be26c1fff7f
Sümeyye KILIÇ <smyyekilic@gmail.com>
2021-02-14 00:16:48 +0300

    rest api delete update ve create işlemleri

--- MyRestApi/myJournall/urls.py
+++ MyRestApi/myJournall/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     path('journalTask-detail/<str:pk>/', views.journalTaskDetail, name="task-detail"),
     path('journalTask-list/', views.journalTaskList, name="journalTask-list"),
     path('journalTask-create/', views.journalTaskCreate, name="journalTask-create"),
-    path('journalTask-update/<str:pk>/', views.journalTaskUpdate, name="journalTask-update"),
-    path('journalTask-delete/<str:pk>/', views.journalTaskDelete, name="journalTask-delete"),
+
 
 ]
 

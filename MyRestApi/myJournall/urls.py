450b0bb247eff53410858bb9172f32818e759cd2
Sümeyye KILIÇ <smyyekilic@gmail.com>
2021-02-13 23:58:27 +0300

    rest api list işlemi

--- MyRestApi/myJournall/urls.py
+++ MyRestApi/myJournall/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.apiView , name="api-view"),
-    path('task-detail/<str:pk>/', views.journalTaskDetail, name="task-detail"),
+    path('journalTask-detail/<str:pk>/', views.journalTaskDetail, name="task-detail"),
+    path('journalTask-list/', views.journalTaskList, name="journalTask-list"),
+    path('journalTask-create/', views.journalTaskCreate, name="journalTask-create"),
+	path('journalTask-update/<str:pk>/', views.journalTaskUpdate, name="journalTask-update"),
+	path('journalTask-delete/<str:pk>/', views.journalTaskDelete, name="journalTask-delete"),
 ]
 
 
--- MyRestApi/myJournall/views.py
+++ MyRestApi/myJournall/views.py
@@ -9,7 +9,11 @@ from .serializers import JournallSerializer
 @api_view(['GET'])
 def apiView(request):
     api_journal_url={
-        'Detail View':'/task-detail/<str:pk>/'
+        'List':'/journalTask-list/',
+        'Detail View':'/journalTask-detail/<str:pk>/',
+        'Create':'/journalTask-create/',
+		'Update':'/journalTask-update/<str:pk>/',
+		'Delete':'/journalTask-delete/<str:pk>/',
     }
 
     return Response(api_journal_url)
@@ -19,5 +23,14 @@ def apiView(request):
 @api_view(['GET'])
 def journalTaskDetail(request, pk):
 	journalTask = bulletJournal.objects.get(id=pk)
-	serializer = bulletJournal(journalTask, many=False)
+	serializer = JournallSerializer(journalTask, many=False)
 	return Response(serializer.data)
+
+
+@api_view(['GET'])
+def journalTaskList(request):
+	journalTask = bulletJournal.objects.all().order_by('-id')
+	serializer = JournallSerializer(journalTask, many=True)
+	return Response(serializer.data)
+
+
